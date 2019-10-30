9ff92c0597e3b46c5662cca48e6d4581a03d43de
sulingling <sll3223848832@163.com>
2019-10-30 10:22:16 +0800

    测试代码

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel54
                 </div>
 
                 <div class="links">
