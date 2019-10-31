518c0e7033edb311cc38dcc25afb078a555ae563
sulingling <sll3223848832@163.com>
2019-10-31 18:19:41 +0800

    提交路由文件

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 文章列表页面
+Route::get('/posts', '\App\Http\Controllers\PostsController@index');
+// 创建文章
+Route::get('/posts/create', '\App\Http\Controllers\PostsController@create');
+Route::post('/posts', '\App\Http\Controllers\PostsController@store');
+// 文章详情页面
+Route::get('/posts/{post}', '\App\Http\Controllers\PostsController@show');
+// 编辑文章
+Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostsController@edit');
+Route::put('/posts/{post}', '\App\Http\Controllers\PostsController@update');
+// 删除文章
+Route::get('/posts/delete', '\App\Http\Controllers\PostsController@delete');
